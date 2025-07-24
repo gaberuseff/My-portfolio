5b751484eddfb6c8bee2eda96043eb4d4407465b
Gaber Youssef <dev.gaber@gmail.com>
2025-07-24 23:32:41 +0300

    fix bug

--- src/app/_components/Home/Hero.js
+++ src/app/_components/Home/Hero.js
@@ -8,9 +8,9 @@ function Hero() {
         <section className="sm:py-16 py-8 px-2 flex flex-col gap-18">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div className="relative w-full aspect-square md:h-full 
-            md:aspect-auto order-1 md:order-2 overflow-hidden rounded-[10%]">
+                    md:aspect-auto order-1 md:order-2 overflow-hidden rounded-[5%]">
                     <Image
-                        src="/me.png"
+                        src="/myimg.webp"
                         alt="Gaber Usef"
                         fill
                         priority
